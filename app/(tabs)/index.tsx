b024cdebda5f47032d7aa29690b3c5f1832ea540
Anthony Pogliano <poglianoster@gmail.com>
2025-05-12 18:17:08 -0600

    Basic button implemented using Pressable utility.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button'
 import ImageViewer from '@/components/ImageViewer'
 import { StyleSheet, View } from 'react-native'
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a Photo' />
+        <Button label='Use This Photo' />
       </View>
     </View>
   )
@@ -20,5 +25,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  imageContainer: { flex: 1 },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 })
---
+++ components/Button.tsx
@@ -0,0 +1,41 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+
+type Props = {
+  label: string
+}
+
+export default function Button({ label }: Props) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert('You pressed the button')}
+      >
+        <Text style={styles.label}>{label}</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 60,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  label: {
+    color: '#fff',
+    fontSize: 16,
+  },
+})
