4562ca1f688cb91bb547d215367bd947701c0b21
Anthony Pogliano <poglianoster@gmail.com>
2025-05-15 11:38:27 -0600

    Added emojisticker and put it in app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,14 @@ import Button from '@/components/Button'
 import CircleButton from '@/components/CircleButton'
 import EmojiList from '@/components/EmojiList'
 import EmojiPicker from '@/components/EmojiPicker'
+import EmojiSticker from '@/components/EmojiSticker'
 import IconButton from '@/components/IconButton'
 import ImageViewer from '@/components/ImageViewer'
 import { type ImageSource } from 'expo-image'
 import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
 
@@ -50,13 +52,16 @@ export default function Index() {
   const onSaveImageAsync = async () => {}
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           imgSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
       </View>
+      {pickedEmoji ? (
+        <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+      ) : null}
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
           <IconButton icon='refresh' label='Save' onPress={onReset} />
@@ -83,7 +88,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   )
 }
 
